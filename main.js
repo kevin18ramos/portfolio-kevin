adf9050abc0cf8f3f01f36534d136259572b37c0
Kevin Ramos <kevin.18ramos04@gmail.com>
2023-01-02 15:02:07 -0600

    added js for bottom contact

---
+++ main.js
@@ -0,0 +1,131 @@
+var linkedin = document.querySelector(".LinkIn");
+var linkedtext = document.querySelector(".Linkedintext");
+var github = document.querySelector(".Github");
+var githubtext = document.querySelector(".Githubtext");
+var resume = document.querySelector(".Resume");
+var resumetext = document.querySelector(".Resumetext");
+
+
+
+linkedin.addEventListener("mouseover", function () {
+    githubtext.style.borderTop = "2px solid #F33A6A";
+    github.style.color = "#F33A6A";
+    githubtext.style.color = "#F33A6A";
+
+    resumetext.style.borderTop = "2px solid #F33A6A";
+    resume.style.color = "#F33A6A";
+    resumetext.style.color = "#F33A6A";
+
+})
+linkedin.addEventListener("mouseleave", function () {
+    githubtext.style.color = "#0CBABA";
+    github.style.color = "#0CBABA";
+    githubtext.style.borderTop = "2px solid #0CBABA";
+
+    resumetext.style.color = "#0CBABA";
+    resume.style.color = "#0CBABA";
+    resumetext.style.borderTop = "2px solid #0CBABA";
+
+})
+
+linkedtext.addEventListener("mouseover", function () {
+    githubtext.style.borderTop = "2px solid #F33A6A";
+    github.style.color = "#F33A6A";
+    githubtext.style.color = "#F33A6A";
+
+    resumetext.style.borderTop = "2px solid #F33A6A";
+    resume.style.color = "#F33A6A";
+    resumetext.style.color = "#F33A6A";
+
+})
+linkedtext.addEventListener("mouseleave", function () {
+    githubtext.style.color = "#0CBABA";
+    github.style.color = "#0CBABA";
+    githubtext.style.borderTop = "2px solid #0CBABA";
+
+    resumetext.style.color = "#0CBABA";
+    resume.style.color = "#0CBABA";
+    resumetext.style.borderTop = "2px solid #0CBABA";
+})
+
+github.addEventListener("mouseover", function () {
+    linkedtext.style.borderTop = "2px solid #F33A6A";
+    linkedin.style.color = "#F33A6A";
+    linkedtext.style.color = "#F33A6A";
+
+    resumetext.style.borderTop = "2px solid #F33A6A";
+    resume.style.color = "#F33A6A";
+    resumetext.style.color = "#F33A6A";
+
+})
+github.addEventListener("mouseleave", function () {
+    linkedtext.style.color = "#0CBABA";
+    linkedin.style.color = "#0CBABA";
+    linkedtext.style.borderTop = "2px solid #0CBABA";
+
+    resumetext.style.color = "#0CBABA";
+    resume.style.color = "#0CBABA";
+    resumetext.style.borderTop = "2px solid #0CBABA";
+
+})
+
+githubtext.addEventListener("mouseover", function () {
+    linkedtext.style.borderTop = "2px solid #F33A6A";
+    linkedin.style.color = "#F33A6A";
+    linkedtext.style.color = "#F33A6A";
+
+    resumetext.style.borderTop = "2px solid #F33A6A";
+    resume.style.color = "#F33A6A";
+    resumetext.style.color = "#F33A6A";
+
+})
+githubtext.addEventListener("mouseleave", function () {
+    linkedtext.style.color = "#0CBABA";
+    linkedin.style.color = "#0CBABA";
+    linkedtext.style.borderTop = "2px solid #0CBABA";
+
+    resumetext.style.color = "#0CBABA";
+    resume.style.color = "#0CBABA";
+    resumetext.style.borderTop = "2px solid #0CBABA";
+})
+
+resume.addEventListener("mouseover", function () {
+    linkedtext.style.borderTop = "2px solid #F33A6A";
+    linkedin.style.color = "#F33A6A";
+    linkedtext.style.color = "#F33A6A";
+
+    githubtext.style.borderTop = "2px solid #F33A6A";
+    github.style.color = "#F33A6A";
+    githubtext.style.color = "#F33A6A";
+
+})
+resume.addEventListener("mouseleave", function () {
+    linkedtext.style.color = "#0CBABA";
+    linkedin.style.color = "#0CBABA";
+    linkedtext.style.borderTop = "2px solid #0CBABA";
+
+    githubtext.style.color = "#0CBABA";
+    github.style.color = "#0CBABA";
+    githubtext.style.borderTop = "2px solid #0CBABA";
+
+})
+
+resumetext.addEventListener("mouseover", function () {
+    linkedtext.style.borderTop = "2px solid #F33A6A";
+    linkedin.style.color = "#F33A6A";
+    linkedtext.style.color = "#F33A6A";
+
+    githubtext.style.borderTop = "2px solid #F33A6A";
+    github.style.color = "#F33A6A";
+    githubtext.style.color = "#F33A6A";
+
+})
+resumetext.addEventListener("mouseleave", function () {
+    linkedtext.style.color = "#0CBABA";
+    linkedin.style.color = "#0CBABA";
+    linkedtext.style.borderTop = "2px solid #0CBABA";
+
+    githubtext.style.color = "#0CBABA";
+    github.style.color = "#0CBABA";
+    githubtext.style.borderTop = "2px solid #0CBABA";
+})
