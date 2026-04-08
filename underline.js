11faa367a35a0122fefbe9bcbfe5f781ade13899
kevin18ramos <kevin.18ramos04@gmail.com>
2026-04-07 23:16:07 -0500

    path correction

--- underline.js
+++ underline.js
@@ -256,7 +256,7 @@ elements.forEach(function(element) {
       date_proj.textContent = linuxdate;
       azurecon.style.fontSize = "6vw";
       cer_click.textContent = "Click here for the repository!";
-      cer_click.href = "https://github.com/kevin18ramos/Linux";
+      cer_click.href = "https://kevin18ramos.github.io/portfolio-kevin/azure.html;
     }
   });
 });
